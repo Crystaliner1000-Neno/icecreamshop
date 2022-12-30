d7b90e54065086d20e55ce8e6f0082cd16e92b85
Crystaliner1000 <119397647+Crystaliner1000@users.noreply.github.com>
2022-12-30 16:19:39 +0100

    Create index.mjs

---
+++ js/index.mjs
@@ -0,0 +1,3 @@
+import { displayIceCream } from "./components/displayProducts.mjs";
+import { default as iceCreams } from "./data/ice-creams.js";
+import { removeItemFromCart } from "./components/removeFromCart.js";
